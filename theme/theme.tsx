1ecfa01593cd6663956d469fe03638a0333f3209
Alfred <alfred@insightcapital.io>
2021-07-06 14:30:16 +0100

    theme colors

--- theme/theme.tsx
+++ theme/theme.tsx
@@ -1,12 +1,10 @@
 const theme = {
     colors: {
-      primary: '#D87D4A',
-      secondary: '#FBAF85',
-      black: '#101010',
-      white: '#FFFFFF',
-      grey: '#C4C4C4',
-      lightGrey: '#F1F1F1',
-      darkGrey: '#7D7D7D',
+      beaver: '#9E7F66',
+      cod: '#111111',
+      mirage: '#17192B',
+      ebony: '#242B37',
+      shuttle: '#5C6779',
     },
     fontSizes: {
       extraLarge: '4rem',
